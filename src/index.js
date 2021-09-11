834a09331d1d6bbd11abe6863693702d40541a30
Igor Vinicius <igorvinicius.oli@gmail.com>
2021-09-11 20:37:26 -0300

    refactor: change status code and remove created_at

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ app.post("/users", (request, response) => {
 
   users.push(userRegister);
 
-  return response.status(201).send();
+  return response.status(201).json(userRegister);
 });
 
 app.get("/todos", checksExistsUserAccount, (request, response) => {
